95e0beca228ec5545bd048ab4d3effd4c8c2dda1
Chad Durkin and Caitlin Hines <caitlin@>
2017-01-10 10:36:24 -0800

    It will have a functioning fadeIn class for each flashcard term

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
+  $("#javascript").click(function() {
+    $("#javascript-def").fadeIn();
+  });
 
+  $("#operators").click(function() {
+    $("#operators-def").fadeIn();
+  });
 
+  $("#variables").click(function() {
+    $("#variables-def").fadeIn();
+  });
+
+  $("#namingVars").click(function() {
+    $("#variablename-def").fadeIn();
+  });
+
+  $("#namingVars").click(function() {
+    $("#variablename-def").fadeIn();
+  });
 });
